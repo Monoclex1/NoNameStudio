4381fa4f3e3895f3542a6d2f8ffecb2bee1fe920
17SchallerJ <17jordanschaller@gmail.com>
2018-01-31 08:27:01 -0500

    Schaller push
    Push of my work for day 1

---
+++ Game1/Assets/Movemet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movemet : MonoBehaviour
+{
+	private int hrizSpeed;
+	private int vertSpeed;
+
+	// Use this for initialization
+	void Start ()
+	{
+		hrizSpeed = 0;
+		vertSpeed = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+}
